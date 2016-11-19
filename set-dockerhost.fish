f0953ffeb7f2fd9f46d3384f59f7c41f3ed6facb
Daniel Neuhäuser <ich@danielneuhaeuser.de>
2016-11-19 13:30:37 +0100

    pick only the first ip in set-dockerhost

--- set-dockerhost.fish
+++ set-dockerhost.fish
@@ -1,2 +1,2 @@
 #!fish
-set -x -g DOCKERHOST (ifconfig | grep "inet " | grep -v "127\.0\.0\.1" | awk '{ print $2 }')
+set -x -g DOCKERHOST (ifconfig | grep "inet " | grep -v "127\.0\.0\.1" | head -1 | awk '{ print $2 }')
